ba0c0dbc7491b12dc3cd2a68fb20f6106c0729a2
Thales <thalesroberto.f@hotmail.com>
2024-11-06 00:11:34 -0300

    first commit

---
+++ Clientes.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atividadeBackCliente
+{
+    public class Clientes
+    {
+        public string nome {get; set;}
+        public string endereco {get; set;}
+        public float valor {get; protected set;}
+        public float valor_imposto {get; protected set;}
+        public float total {get; protected set;}
+        public virtual void Pagar_Imposto(float v)
+        {
+            this.valor = v;
+            this.valor_imposto = this.valor * 10 / 100;
+            this.total = this.valor + this.valor_imposto;
+        }
+    }
+}
---
+++ Pessoa_Fisica.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atividadeBackCliente
+{
+    public class Pessoa_Fisica : Clientes
+    {
+        public string cpf {get; set;}
+        public string rg {get; set;}
+    }
+}
